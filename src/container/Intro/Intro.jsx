229dbed29ccc5ad1f32618d88ce025e4d205d97c
Marshallhch <marshallhch@gmail.com>
2022-03-02 18:56:34 +0900

    images gallery...

--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -1,11 +1,45 @@
-import React from 'react';
+import React from "react";
+import { BsFillPlayFill, BsPauseFill } from "react-icons/bs";
+import { meal } from "../../constants";
 
-import './Intro.css';
+import "./Intro.css";
 
-const Intro = () => (
-  <div>
-    Intro
-  </div>
-);
+const Intro = () => {
+  const [playVideo, setPlayVideo] = React.useState(false);
+  const vidRef = React.useRef(); // hook 사용 시 특정 요소 선택하여 저장 : https://react.vlpt.us/basic/10-useRef.html
+
+  const handleVideo = () => {
+    setPlayVideo((prevPlayVideo) => !prevPlayVideo);
+    if (playVideo) {
+      vidRef.current.pause();
+    } else {
+      vidRef.current.play();
+    }
+  };
+
+  return (
+    <div className='app__video'>
+      <video
+        src={meal}
+        ref={vidRef}
+        type='video/mp4'
+        loop
+        controlls='false'
+        muted
+      />
+      <div className='app__video-overlay flex__center'>
+        <div
+          className='app__video-overlay_circle flex__center'
+          onClick={handleVideo}>
+          {playVideo ? (
+            <BsPauseFill color='#fff' fontSize={30} />
+          ) : (
+            <BsFillPlayFill color='#fff' fontSize={30} />
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Intro;
